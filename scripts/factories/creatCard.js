57c46c1ed61f50dd9668ac09fda18bf749736786
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2022-06-22 16:16:51 +0200

    modification du dom js

--- scripts/factories/creatCard.js
+++ scripts/factories/creatCard.js
@@ -1,63 +1,84 @@
 /* eslint-disable no-unused-vars */
-class RecipesCard{
-    constructor(recipe){
-        this.id = recipe.id;
-        this.name = recipe.name;
-        this.servings = recipe.servings;
-        this.ingredients = recipe.ingredients;        
-        this.time = recipe.time;
-        this.description = recipe.description;
-        this.appliance = recipe.appliance.toLowerCase();//La méthode toLowerCase() retourne la chaîne de caractères courante en minuscules.
-              
-    }    
+class RecipesCard {
+  constructor(recipe) {
+    this.id = recipe.id;
+    this.name = recipe.name;
+    this.servings = recipe.servings;
+    this.ingredients = recipe.ingredients;
+    this.time = recipe.time;
+    this.description = recipe.description;
+    this.appliance = recipe.appliance.toLowerCase(); //La méthode toLowerCase() retourne la chaîne de caractères courante en minuscules.
+  }
 
-    buildCard() {
-        const card = document.createElement("article");
-        console.log(card);
-        let photo = "";
-        photo = this.name.toLowerCase().replace(/\s/g, "");
-    
-        card.classList.add(
-          "card",
-          "col-lg-4",
-          "col-md-6",
-          "col-sm-12",
-          "g-4",
-          "border-0"
-        );
-        card.innerHTML = `
-          
+  buildCard() {
+    const card = document.createElement("article");
+    console.log(card);
+    let photo = "";
+    photo = this.name.toLowerCase().replace(/\s/g, "");
+
+    card.classList.add(
+      "card",
+      "col-lg-4",
+      "col-md-6",
+      "col-sm-12",
+      "g-4",
+      "border-0"
+    );
+
+    card.innerHTML = `          
             <img class="card-img-top"  src="assets/images/${photo}.jpg"
-        }" alt="${this.name}" />
-            <div   class="card-body py-0">
-                <div id="receipTitle" class="d-flex flex-row mt-2 mb-2 gap-2 justify-content-between align-items-center">
-                    <h2 class="card-title col mt-0">${this.name}</h2>
-                    <div class="time col-4 d-flex flex-row align-items-center justify-content-end">
-                        <p class="mr-2 minutes">
-                            <span class="me-2">
-                            <img  src="assets/svg/time.svg" alt="">
-                            </span>${this.time} min
-                        </p>
-                    </div>
-                </div>
-            
-                <div class="recipe d-flex flex-row justify-content-between gap-1">
-                    <ul class="ingredient-container col-5 mb-0">${this.ingredients
-                      .map(
-                        (element) =>
-                          `
-                    <li>
-                        <strong>${element.ingredient} :</strong> ${
-                            "quantity" in element ? element.quantity : ""
-                          } ${"unit" in element ? element.unit : ""}
-                    `
-                      )
-                      .join("")}</li>
-                    </ul>
-                    <p class="card-text col-7 m-0">${this.description}</p>
-                </div>
-            </div>`;
-    
-        return card;
-      }
-}//console.log(RecipesCard);
+        }" alt="${this.name}" />`;
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body py-0";
+    card.appendChild(cardBody);
+
+    let recipeInfo = document.createElement('div');
+  recipeInfo.className = 'receipTitle d-flex flex-row mt-2 mb-2 gap-2 justify-content-between align-items-center';
+  cardBody.appendChild(recipeInfo);
+
+  let cardTitle = document.createElement('h2');
+  cardTitle.className = 'card-title col mt-0';
+  cardTitle.textContent = `${this.name}`;
+  recipeInfo.appendChild(cardTitle);
+
+  let timeIcon = document.createElement('div');
+  timeIcon.className = 'time col-4 d-flex flex-row align-items-center justify-content-end';
+  recipeInfo.appendChild(timeIcon);
+
+  let recipeIconAndTime = document.createElement('span')
+  recipeIconAndTime.className = 'mr-2 minutes';
+
+  let icon = document.createElement('i');
+  icon.className = 'far fa-clock';
+  recipeIconAndTime.appendChild(icon);
+  recipeIconAndTime.textContent = `${this.time}`+'min';
+
+  timeIcon.appendChild(recipeIconAndTime);
+
+  let recipeInfoSecond = document.createElement('div');
+  recipeInfoSecond.className = 'recipe d-flex flex-row justify-content-between gap-1';
+
+  cardBody.appendChild(recipeInfoSecond);
+
+  recipeInfoSecond.innerHTML = `<ul class="ingredient-container col-5 mb-0">${this.ingredients
+    .map(
+      (element) =>
+        `
+  <li>
+      <strong>${element.ingredient} :</strong> ${
+          "quantity" in element ? element.quantity : ""
+        } ${"unit" in element ? element.unit : ""}
+  `
+    )
+    .join("")}</li>
+  </ul>`
+
+  let etape = document.createElement('p');
+  etape.className = 'card-text col-7 m-0';
+  etape.textContent = `${this.description}`;
+
+  recipeInfoSecond.appendChild(etape);
+    return card;
+  }
+} //console.log(RecipesCard);
